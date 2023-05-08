30d8dba219e548361677f1a5f6ff6aaadaac79ad
Somanyu <somanyu.03samal@gmail.com>
2023-05-08 19:52:35 +0530

    add: ESLint the repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<h1 className="text-3xl font-bold underline text-center text-amber-800">
+			Hello world!
+		</h1>
+	);
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require("flowbite/plugin")
+	],
+};
